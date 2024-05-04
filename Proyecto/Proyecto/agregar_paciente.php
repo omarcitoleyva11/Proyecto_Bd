473e20758e29a25bf3c94017653dfd56bf7315e6
Omarcitoleyva11 <joahanomarleyvaperez@gmail.com>
2024-05-04 16:37:57 -0700

    listo

--- Proyecto/Proyecto/agregar_paciente.php
+++ Proyecto/Proyecto/agregar_paciente.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "localhost";
     $username = "root";
-    $password = "v10203040D:";
+    $password = "Joahan11.";
     $database = "mydb";
     $port = 3306;
 
@@ -18,25 +18,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $correo = clean_input($_POST["correo"], $conn);
     $id_seguro = clean_input($_POST["id_seguro"], $conn);
 
-    $stmtSeguro = $conn->prepare("CALL sp_AgregarSeguro(?, ?, ?, ?, ?)");
-    $stmtSeguro->bind_param("ssiss", $nombre, $direccion, $telefono, $correo,$id_seguro);
 
-    if ($stmtSeguro->execute()) {
-        echo "Datos insertados con éxito en la tabla Seguro<br>";
+    if (!empty($id_seguro)) {
+        $stmtSeguro = $conn->prepare("CALL sp_AgregarSeguro(?, ?, ?, ?, ?)");
+        $stmtSeguro->bind_param("ssiss", $nombre, $direccion, $telefono, $correo, $id_seguro);
+
+        if ($stmtSeguro->execute()) {
+            $seguro_message = "Datos insertados con éxito en la tabla Seguro";
+        } else {
+            $seguro_message = "Error al insertar en la tabla Seguro: " . $conn->error;
+        }
     } else {
-        echo "Error al insertar en la tabla Seguro: " . $conn->error;
+        $seguro_message = "El campo ID_SEGURO está vacío, no se realizó la inserción en la tabla Seguro";
     }
 
+
     $stmtPaciente = $conn->prepare("CALL sp_AgregarPaciente(?, ?, ?, ?, ?)");
-    $stmtPaciente->bind_param("ssiss", $nombre, $direccion, $telefono, $correo, $id_seguro);
+
+    $id_seguro_paciente = !empty($id_seguro) ? $id_seguro : NULL;
+    $stmtPaciente->bind_param("ssiss", $nombre, $direccion, $telefono, $correo, $id_seguro_paciente);
 
     if ($stmtPaciente->execute()) {
-        echo "Paciente insertado con éxito";
+        $paciente_message = "Paciente insertado con éxito";
     } else {
-        echo "Error al insertar el paciente: " . $conn->error;
+        $paciente_message = "Error al insertar el paciente: " . $conn->error;
     }
 
     $conn->close();
+
+
+    $response = array(
+        'seguro_message' => $seguro_message,
+        'paciente_message' => $paciente_message
+    );
+
+
+    echo json_encode($response);
 }
 
 function clean_input($data, $conn) {
